dbd2491dae3b5ae3d77a33bd6ccb89aff912a4c4
TryTooFly <94642479+TryTooFly@users.noreply.github.com>
2022-08-01 11:52:45 +0000

    Deploying to gh-pages from @ TryTooFly/goit-js-hw-09@e96c3d023f8856670cd624798efa115c5a715d10 🚀

---
+++ 01-color-switcher.c3828b60.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){o.style.backgroundColor=clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.c3828b60.js.map
