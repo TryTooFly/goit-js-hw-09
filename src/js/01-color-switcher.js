e96c3d023f8856670cd624798efa115c5a715d10
Serhiy Shvayka <sergiyshvayka@gmail.com>
2022-08-01 14:52:10 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener('click', startChanging);
+stopBtn.addEventListener('click', stopChanging);
+
+function startChanging() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+}
+
+function stopChanging() {
+  body.style.backgroundColor = clearInterval(timerId);
+
+  startBtn.disabled = false;
+}
